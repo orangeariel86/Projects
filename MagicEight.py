c2eb3874e81ce39bb7b448b193d3493de79e0e30
orangeariel86 <73204134+orangeariel86@users.noreply.github.com>
2020-10-23 14:20:21 -0700

    Add files via upload

---
+++ MagicEight.py
@@ -0,0 +1,16 @@
+#Madeby:Tzu-Han,Hsu
+with open("8_ball_responses.txt","r") as myFile:
+    lines1 = []
+    lines2 = []
+    for i in myFile:
+        lines = myFile.readlines()
+        for i in lines:
+            lines2.append(i.strip("\n"))
+
+import random
+
+
+while True:
+    user = input("Ask a question")
+    rannum = random.randint(0,len(lines)-1)
+    print(lines2[rannum])
